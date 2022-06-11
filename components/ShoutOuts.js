bf71fc939789f5edc2be808b7aa88b713d0f0f8c
Joe Puzzo <jpuzzo@tesla.com>
2022-06-11 04:53:11 -0700

    cleaned up shoutouts

--- components/ShoutOuts.js
+++ components/ShoutOuts.js
@@ -9,13 +9,10 @@ export const ShoutOuts = () => {
     </StatusMessage>
 
     <StatusMessage alwaysShow title="Pahel Makhija  - June 9, 2022">
-
-    Pahel Makhija recognized Joe with a digital Innovative Coin!
-    Joe is an invaluable member of the team who contributes greatly to Tesla's pricing product as well libraries used across the company. He has materialized the vision of barker becoming a truly self-serve tool, through integrating self-serve regression and approval via email to make it easier for execs to approve price changes. His passion for Tesla allows him to go above and beyond as demonstrated by him taking initiative to create a Tesla wide approval service adding value company wide.
+      Joe is an invaluable member of the team who contributes greatly to Tesla's pricing product as well libraries used across the company. He has materialized the vision of barker becoming a truly self-serve tool, through integrating self-serve regression and approval via email to make it easier for execs to approve price changes. His passion for Tesla allows him to go above and beyond as demonstrated by him taking initiative to create a Tesla wide approval service adding value company wide.
     </StatusMessage>
 
     <StatusMessage alwaysShow title="Matt Sadauckas - Jun 7, 2022">
-      Matt Sadauckas recognized Joe with a digital Dependable Coin!
       Joe's form library Informed has become an absolute lynchpin to my applications and I'm sure to many others. With that accomplishment, Joe has embraced the responsibility to be available to his users to fix issues and help them through complex use cases to an absolutely exemplary degree. No one else I've worked with at Tesla is as responsive or generous with their time, and this is a pretty high bar! Thanks, as always!
     </StatusMessage>
 
@@ -24,53 +21,43 @@ export const ShoutOuts = () => {
     </StatusMessage>
 
     <StatusMessage alwaysShow title="Ali Stump - Mar 2, 2022">   
-      Ali Stump recognized Joe with a digital Optimistic Leadership Coin!
       Joe saw a conversation getting stressed and jumped in with humor to diffuse the situation. It helped everyone get a more level head and reengage for a more positive conclusion.
     </StatusMessage>
 
     <StatusMessage alwaysShow title="Jennifer Culp - Jan 11, 2022">   
-      Jennifer Culp recognized Joe with a digital Driven Coin!
       To say that Joe has no quit would be an understatement. He understands what needs to be done and is skilled in finding the balance between what *must* be done vs. what *should* be done. Joe has the ability to be a huge guiding force in code quality and standards across the organization.
     </StatusMessage>
 
     <StatusMessage alwaysShow title="Ashwinkumar Venkitakrishnan - Dec 18, 2021">   
-      Ashwinkumar Venkitakrishnan recognized Joe with a digital Innovative Coin!
       Joe's work with React context form has been phenomenal. True to a library maintainer's ideals, he has provided great value, along with great support to consumers. His upgrades to rcf have never left a consumer stuck in a hard spot. Support for cjs along with esm is something he didn't have to do, but he still did, allowing consumers the right time to update their repos instead of enforcing it on them.
     </StatusMessage>
 
     <StatusMessage alwaysShow title="Matt Gross - Sep 20, 2021">   
-      Matt Gross recognized Joe with a digital Driven Coin!
       Joe jumped on the new requirements for intl phone inputs, all the way from the forms level to the package/data-work & knocked it out crazy fast
     </StatusMessage>
 
     <StatusMessage alwaysShow title="Matt Gross - Jan 13, 2021">   
-      Matt Gross recognized Joe with a digital Driven Coin!
       Joe's a crazy tank. He runs very hard & is crazy passionate about excellent work.
     </StatusMessage>
 
     
     <StatusMessage alwaysShow title="Jennifer Culp - Jan 13, 2021">   
-      Jennifer Culp recognized Joe with a digital Dependable Coin!
       Joe's work is always high quality. He is dedicated to doing things right even when it's hard. His passion and creativity leads to great things!
     </StatusMessage>
 
     <StatusMessage alwaysShow title="Dejan Celojevic - Dec 14, 2020">   
-      Dejan Celojevic recognized Joe with a digital Innovative Coin!
       Excellent engineer, comes up with efficient and innovative ideas to tackle problems. Very helpful!
     </StatusMessage>
 
     <StatusMessage alwaysShow title="Matthew Pelosi  - Dec 14, 2020">   
-      Matthew Pelosi recognized Joe with a digital Innovative Coin!
       It's incredible the value Joe has brought to Tesla with his contributions to the React-Context-Form Library. I can't say enough good things about Joe!
     </StatusMessage>
 
     <StatusMessage alwaysShow title="Luc Martin - Dec 14, 2020">   
-      Luc Martin recognized Joe with a digital Innovative Coin!
       Thank you Joe for teaching all of us how to use this invaluable tool that is RCF. Your teaching are really excellent and useful for all of us UI devs
     </StatusMessage>
 
     <StatusMessage alwaysShow title="Luc Martin - Aug 28, 2020">   
-      Luc Martin recognized Joe with a digital Dependable Coin!
       Joe has done amazing work on a component we all use: React Context form. HIs collaborative effort has been a great improvement for all teams.
     </StatusMessage>
 
