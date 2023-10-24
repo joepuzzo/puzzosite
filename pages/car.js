772821f7d304c7b98b7f0b72d08da2e1897b768a
joepuzzo <jgpuzzo2@gmail.com>
2023-10-23 21:06:28 -0700

    added ability to have moving car

--- pages/car.js
+++ pages/car.js
@@ -3,10 +3,13 @@ import styles from "../styles/Home.module.css";
 import BubbleBackground from "../components/BubbleBackground";
 import { Car } from "../components/CarColor";
 import { useRouter } from "next/router";
+import { useState } from "react";
 
 export default function CarPage() {
   const router = useRouter();
   const { title, description } = router.query;
+
+  const [move, setMove] = useState(false);
 
   return (
     <div className={styles.container}>
@@ -20,9 +23,19 @@ export default function CarPage() {
           content="https://www.joepuzzo.com/imagelink.png"
         />
       </Head>
-      <h1 className={styles.title}>{title}</h1>
-      <p className="description">{description}</p>
-      <Car value="red" />
+      <div className={`flex-column ${move ? "pop-and-disappear" : ""}`}>
+        <h1 className={styles.title}>{title}</h1>
+        <p className="description">{description}</p>
+      </div>
+      <div className={`${move ? "moving-div" : "entering-div"}`}>
+        <Car value="red" />
+      </div>
+      <button
+        className={move ? "pop-and-disappear" : ""}
+        onClick={() => setMove((prev) => !prev)}
+      >
+        Lets Go!
+      </button>
     </div>
   );
 }
