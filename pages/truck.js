c172725cf1059ecb2ca10881101ea5e681a7da5d
joepuzzo <jgpuzzo2@gmail.com>
2023-11-28 07:33:37 -0800

    added cybertruck page

---
+++ pages/truck.js
@@ -0,0 +1,50 @@
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import BubbleBackground from "../components/BubbleBackground";
+import { Cybertruck } from "../components/Cybertruck";
+import { useRouter } from "next/router";
+import { useState } from "react";
+
+export default function CarPage() {
+  const router = useRouter();
+  const { title, description, href } = router.query;
+
+  const [move, setMove] = useState(false);
+
+  const handleButtonClick = () => {
+    setMove((prev) => !prev);
+    setTimeout(() => {
+      window.location.href = href;
+    }, 4000); // Delay of 4 seconds
+  };
+
+  return (
+    <div className={styles.container}>
+      <BubbleBackground />
+      <Head>
+        <title>Joe Puzzo</title>
+        <meta name="description" content="Kick Ass Developer" />
+        <link rel="icon" href="/favicon.ico" />
+        <meta
+          property="og:image"
+          content="https://www.joepuzzo.com/imagelink.png"
+        />
+      </Head>
+      <div className={`flex-column ${move ? "pop-and-disappear" : ""}`}>
+        <h1 className={styles.title}>{title}</h1>
+        <p className="description">{description}</p>
+      </div>
+      <div>
+        <Cybertruck />
+      </div>
+      {href ? (
+        <button
+          className={move ? "pop-and-disappear" : ""}
+          onClick={handleButtonClick}
+        >
+          Lets Go!
+        </button>
+      ) : null}
+    </div>
+  );
+}
